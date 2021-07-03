6708b7cfe40794ad649bfc322df857ef89cd28f6
Liam Neeson <shabekuri7@naver.com>
2021-07-03 11:45:57 +0900

    Initial commit

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    //private float visiblePosZ = -6.5f;
+
+    private int largeCloudScore = 20;
+    private int smallCloudScore = 10;
+    private int largeStarScore = 15;
+    private int smallStarScore = 5;
+
+    //private Text scoreText;
+    private int score = 0;
+
+    private GameObject scoreBoardText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreBoardText = GameObject.Find("ScoreBoard") ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreBoardText.GetComponent<Text>().text = string.Format("Score:{0}",this.score);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string tag = collision.gameObject.tag;
+        
+        if (tag == "LargeCloudTag")
+        {
+            this.score += largeCloudScore;
+            Debug.Log("score"+largeCloudScore);
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            this.score += smallCloudScore;
+            Debug.Log("score"+smallCloudScore);
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.score += largeStarScore;
+            Debug.Log("score"+largeStarScore);
+        }
+        else if (tag == "SmallStarTag")
+        {
+            this.score += smallStarScore;
+            Debug.Log("score"+smallStarScore);
+        }
+    }
+}
