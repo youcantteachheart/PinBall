8f4aefeced8d3b43b8885bb6cd5d8440c720a16b
Liam Neeson <shabekuri7@naver.com>
2021-07-03 16:01:00 +0900

    キーボード操作機能追加

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -19,20 +19,30 @@ public class FripperController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        //https://docs.unity3d.com/ja/2019.4/ScriptReference/KeyCode.html
+        
+        if((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && tag == "LeftFripperTag")
         {
             SetAngle(this.flickAngle);
         }
-        if(Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        if((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        if((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && (tag == "LeftFripperTag" || tag == "RightFripperTag"))
         {
             SetAngle(this.flickAngle);
         }
 
-        if(Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        if((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)) && tag == "LeftFripperTag")
         {
             SetAngle(this.defaultAngle);
         }
-        if(Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        if((Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if((Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) && (tag == "LeftFripperTag" || tag == "RightFripperTag"))
         {
             SetAngle(this.defaultAngle);
         }
